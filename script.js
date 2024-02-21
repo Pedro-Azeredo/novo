3aebd4bbb9be78a609af188f111ad2c0d5875675
Aluno do Unilasalle <aluno.unilasalle@acad.nit>
2024-02-21 12:00:49 -0300

    aula 21/02/2024

--- script.js
+++ script.js
@@ -1,2 +1,47 @@
-console.log("hello fucking word")
-console.log("git < postgree")
+// let nota1 = +(prompt('digite a nota 1'));
+// let nota2 = +(prompt('digite a nota 2'));
+// let nota3 = +(prompt('digite a nota 3'));
+
+// let media = (nota1 + nota2 + nota3)/3;
+// console.log(media);
+
+// const btn= document.querySelector("button");
+// window.addEventListener("keydown", clicar)
+
+// function clicar(event) {
+//     console.log(event.key);
+// }
+
+// var v = 64 //base
+// var z = 3 //indice
+
+// document.write(`O valor de ${v} elevado a ${z} é igual: ${Math.pow(v, z)}<br>`) 
+// document.write(`A raiz de ${v} com indice ${z} é igual:  ${Math.pow(v, 1/z)}<br>`)
+// document.write(`A raiz quadrada de ${v} é igual: ${Math.sqrt(v ,2).toFixed(1)}`)
+
+
+
+// var nome = prompt("Qual o seu nome?");
+// document.write(`Bem vindo a La salle, ` + nome);
+
+
+// var numero1 = +prompt("Digite número 1");
+// var numero2 = +prompt("Digite número 2");
+// document.write(` ${numero1} +  ${numero2} = ${ numero1 + numero2 }<br>`)
+// document.write(` ${numero1} *  ${numero2} = ${ numero1 * numero2 }<br>`)
+// document.write(` ${numero1} /  ${numero2} = ${ (numero1 / numero2).toFixed(2) } <br>`)
+// document.write(` ${numero1} -  ${numero2} = ${ numero1 - numero2 }<br>`)
+ 
+
+// var nota1 = +prompt("Digite nota 1");
+// var nota2 = +prompt("digite nota 2");
+// var media = (nota1 + nota2)/2;
+// document.write(media);
+
+
+// var um = 5
+// var dois = 3
+// var resul = (um - dois)
+
+// document.write(`O resultado da subtração é: ${resul}`)
+
